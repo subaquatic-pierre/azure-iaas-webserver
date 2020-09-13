aec6f383a1e38a0a18b6c145839860e55650155e
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-09-13 13:12:07 +0400

    start project

---
+++ main.tf
@@ -0,0 +1,173 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+
+}
+
+// Main virtual network, subnets, nics and public IPs
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-internal-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/16"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = var.instance_count
+  name                = "${var.prefix}-nic-${count.index}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "${var.prefix}-primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
+
+// Secutiy Group
+
+resource "azurerm_network_security_group" "webserver" {
+  name                = "${var.prefix}-webserver-sg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "${var.prefix}-allow-internal"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = azurerm_subnet.internal.address_prefix
+    destination_port_range     = "*"
+    destination_address_prefix = azurerm_subnet.internal.address_prefix
+  }
+
+  security_rule {
+    access                     = "Deny"
+    direction                  = "Inbound"
+    name                       = "${var.prefix}-block-external"
+    priority                   = 101
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "*"
+    destination_address_prefix = azurerm_subnet.internal.address_prefix
+  }
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
+
+// Load balancer
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "${var.prefix}-public-address"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
+
+resource "azurerm_lb_nat_rule" "main" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "${var.prefix}-https-access"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = azurerm_lb.main.frontend_ip_configuration[0].name
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "${var.prefix}-backend-address-pool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+  count                   = var.instance_count
+  backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+  ip_configuration_name   = "${var.prefix}-primary"
+  network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                = "${var.prefix}-avset"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  count                           = var.instance_count
+  name                            = "${var.prefix}-vm-${count.index}"
+  resource_group_name             = azurerm_resource_group.main.name
+  source_image_id                 = var.image_id
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D3_v2"
+  admin_username                  = var.username
+  admin_password                  = var.password
+  availability_set_id             = azurerm_availability_set.avset.id
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.main[count.index].id,
+  ]
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  tags = {
+    udacity = "${var.prefix}-project-1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "udacity"
+}
+
+variable "username" {
+  description = "Enter username to associate with the machine"
+  default     = "udacity"
+
+}
+
+variable "password" {
+  description = "Enter password to use to access the machine"
+  default     = "Udacity123"
+
+}
+
+variable "instance_count" {
+  description = "Enter number of vm instances for this configuration"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "eastus"
+}
+
+variable "image_id" {
+  description = "Enter the ID for the image which will be used for craeting the Virtual Machines"
+  default     = "/subscriptions/fce5d291-15d7-436d-a736-05219bee3b1e/resourceGroups/udacity-rg/providers/Microsoft.Compute/images/project1-image"
+}
